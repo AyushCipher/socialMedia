c74b6fc033c9f06b7d9c9464d072711a7bb8807a
Ayush Verma <162732564+AyushCipher@users.noreply.github.com>
2025-08-18 23:49:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="srv-d2hmbbggjchc73a8576g"
+export const serverUrl="https://socialmedia-backend-oz0l.onrender.com"
 function App() {
    getCurrentUser()
    getSuggestedUsers()
